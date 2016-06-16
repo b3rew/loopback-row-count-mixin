e833d4c389ff1c334476bf693f2d632bbd298f80
berew <brkt13@gmail.com>
2016-06-16 17:36:05 +0300

    row count for empty response

--- counts.js
+++ counts.js
@@ -1,12 +1,18 @@
 module.exports = function RowCount (Model) {
   'use strict';
 
+  // Model.afterRemote('findById', injectCounts);
+  // Model.afterRemote('findOne', injectCounts);
   Model.afterRemote('find', injectCounts);
 
   function injectCounts (ctx, unused, next) {
     var resources = ctx.result;
     if (!Array.isArray(resources)) resources = [resources];
     if (!resources.length) {
+        ctx.result = {
+            count: 0,
+            rows: []
+        };
       return next();
     }
     var filter = ctx.args && ctx.args.filter && ctx.args.filter.where ? ctx.args.filter.where : {};
@@ -17,7 +23,6 @@ module.exports = function RowCount (Model) {
                 rows: resources
             };
         }else{
-            console.log(err);
             return next(err);
         }
 
